34d8727a69da5caa4962986ff408fd4c142c8b8d
Loan <loan.benedetto@gmail.com>
2024-10-16 12:04:26 +0200

    je suis bloqué et je peux pas me débloquer  tout seul donc bah je suis que là
    tentative d api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 async function getData() {
-    const pokedex = await fetch("https://hp-api.onrender.com/api/characters")
-        .then((response) => response.json())
-        .catch((error) => alert("Erreur : " + error));
-    
+    try {
+        const response = await fetch("https://hp-api.onrender.com/api/characters");
+        const personnages = await response.json();
+
+        const main = document.querySelector("main");
+
+        for (const personnage of personnages) {
+            let article = document.createElement("article");
+
+            article.innerHTML = `
+                <div class="${personnage.house.toLowerCase()}">
+                    <img src="${personnage.image}" class="image_hover" alt="${personnage.name}">
+                    <p style="color: var(--Gold, #b99049)"> "${personnage.name}" </p>
+                </div>
+            `;
+
+            main.appendChild(article);
+        };
+
+    } catch (error) {
+        alert("Erreur lors de la récupération des données: " + error);
+    }
+}
+
+getData();
